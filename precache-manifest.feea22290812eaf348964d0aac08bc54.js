0aaa85d3c93ec7769e7513d3b4e594155a332ff6
Jouni Varis <jjvaris@gmail.com>
2019-04-14 17:48:25 +0300

    Updates

---
+++ precache-manifest.feea22290812eaf348964d0aac08bc54.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "41e3b43b921eb649778b",
+    "url": "/kosmetologi-kotiisi-frontend/static/css/main.4ebc45b7.chunk.css"
+  },
+  {
+    "revision": "41e3b43b921eb649778b",
+    "url": "/kosmetologi-kotiisi-frontend/static/js/main.432654e4.chunk.js"
+  },
+  {
+    "revision": "56b376697fb3b5d4f126",
+    "url": "/kosmetologi-kotiisi-frontend/static/js/runtime~main.543c56b3.js"
+  },
+  {
+    "revision": "ce26a3b8c9dcbc3654d0",
+    "url": "/kosmetologi-kotiisi-frontend/static/js/2.64a2a2d9.chunk.js"
+  },
+  {
+    "revision": "aa01fcc3ce31b4704c089ffff2dca165",
+    "url": "/kosmetologi-kotiisi-frontend/static/media/niina.aa01fcc3.jpg"
+  },
+  {
+    "revision": "27567695161f1bd65443018f26609725",
+    "url": "/kosmetologi-kotiisi-frontend/static/media/logo.27567695.jpg"
+  },
+  {
+    "revision": "61bff86d3ea0414d37a68381c69dcea4",
+    "url": "/kosmetologi-kotiisi-frontend/static/media/tuotteet.61bff86d.jpg"
+  },
+  {
+    "revision": "fcbc7accbdde869a514a0b85ab77d315",
+    "url": "/kosmetologi-kotiisi-frontend/static/media/2.fcbc7acc.jpg"
+  },
+  {
+    "revision": "10f3ed4c0b4a8f8a92c4e80f8f73f38e",
+    "url": "/kosmetologi-kotiisi-frontend/static/media/14.10f3ed4c.jpg"
+  },
+  {
+    "revision": "49e6844d717d7316f2469f7e62f68591",
+    "url": "/kosmetologi-kotiisi-frontend/static/media/lady2.49e6844d.jpg"
+  },
+  {
+    "revision": "326b42ccab17da2c22fb824188584fd2",
+    "url": "/kosmetologi-kotiisi-frontend/static/media/sokerointi.326b42cc.jpg"
+  },
+  {
+    "revision": "ff940aeedc6cfa27ffab6bfb850a52e5",
+    "url": "/kosmetologi-kotiisi-frontend/static/media/i_1.ff940aee.jpg"
+  },
+  {
+    "revision": "22aac59ac215d0f9bcf4add83fa3fc8c",
+    "url": "/kosmetologi-kotiisi-frontend/static/media/i_2.22aac59a.jpg"
+  },
+  {
+    "revision": "d11a7461f3c84dc1da9fab83c21e3e07",
+    "url": "/kosmetologi-kotiisi-frontend/index.html"
+  }
+];
