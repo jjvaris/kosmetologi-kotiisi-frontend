949f6eeac4e2c32f6ae40380ebd27d289a6cb231
Jouni Varis <jjvaris@gmail.com>
2019-04-27 11:20:04 +0300

    Updates

--- precache-manifest.0afb7afd18c088dea66309987730276b.js
+++ precache-manifest.0afb7afd18c088dea66309987730276b.js
@@ -1,19 +1,23 @@
 self.__precacheManifest = [
   {
-    "revision": "aadc3e1a51215274a07c",
-    "url": "/static/css/main.34abd96b.chunk.css"
+    "revision": "8de275da623d9e09d398",
+    "url": "/static/css/main.8c091c3d.chunk.css"
   },
   {
-    "revision": "aadc3e1a51215274a07c",
-    "url": "/static/js/main.6f840450.chunk.js"
+    "revision": "8de275da623d9e09d398",
+    "url": "/static/js/main.f618c760.chunk.js"
   },
   {
     "revision": "fdfcfda2d9b1bf31db52",
     "url": "/static/js/runtime~main.c5541365.js"
   },
   {
-    "revision": "39ebeb2e6e83b4d05375",
-    "url": "/static/js/2.e869e7c8.chunk.js"
+    "revision": "13b39ca5f982fad86aa1",
+    "url": "/static/js/2.bbc4f296.chunk.js"
+  },
+  {
+    "revision": "febde53b3c0c8759cf0beefaa4cf93b0",
+    "url": "/static/media/jane_iredale.febde53b.jpg"
   },
   {
     "revision": "27567695161f1bd65443018f26609725",
@@ -36,7 +40,7 @@ self.__precacheManifest = [
     "url": "/static/media/products.cab7f1f2.jpg"
   },
   {
-    "revision": "5f0fd82a2d5ff15f3904aa58a65b74ff",
+    "revision": "f91a10e37f7ed68b8ed8e6861e558656",
     "url": "/index.html"
   }
 ];
